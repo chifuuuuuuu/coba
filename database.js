76dd6e8a5999634e29c0c3ebf044ab1e26c1b25c
chifuuuuuuu <161667941+chifuuuuuuu@users.noreply.github.com>
2024-09-25 18:09:56 +0700

    Add files via upload

--- database.js
+++ database.js
@@ -1,70 +1,48 @@
-import { resolve, dirname as _dirname } from 'path'
-import _fs, { existsSync, readFileSync } from 'fs'
-const { promises: fs } = _fs
+import fs from 'fs'
+import moment from 'moment-timezone'
 
-class Database {
-    /**
-     * Create new Database
-     * @param {String} filepath Path to specified json database
-     * @param  {...any} args JSON.stringify arguments
-     */
-    constructor(filepath, ...args) {
-        this.file = resolve(filepath)
-        this.logger = console
-        
-        this._load()
+let handler = async (m, { usedPrefix, command, conn, text }) => {
+  let mentionedJid = [m.sender]
+let name = conn.getName(m.sender)
+let reyz = { key : {
+           participant : '0@s.whatsapp.net'
+                        },
+       message: {
+                    locationMessage: {
+                    name: 'Japan`s',
+                    jpegThumbnail: fs.readFileSync('./thumbnail.jpg')
+                          }
+                        }
+                      }
+    let totalreg = Object.keys(global.db.data.users).length
+    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
+    let kon = `*Database Saat Ini ${totalreg} User*\n*Terdaftar Saat Ini ${rtotalreg} User*`
+let loadd = [
+ '《██▒▒▒▒▒▒▒▒▒▒▒》10%',
+ '《███████▒▒▒▒▒▒》50%',
+ '《█████████████》100%',
+ '𝙻𝙾𝙰𝙳𝙸𝙽𝙶 𝙲𝙾𝙼𝙿𝙻𝙴𝚃𝙴𝙳...'
+ ]
 
-        this._jsonargs = args
-        this._state = false
-        this._queue = []
-        this._interval = setInterval(async () => {
-          if (!this._state && this._queue && this._queue[0]) {
-            this._state = true
-            await this[this._queue.shift()]().catch(this.logger.error)
-            this._state = false
-          }
-        }, 1000)
-        
-    }
+let { key } = await conn.sendMessage(m.chat, {text: '_Sedang Loading_'})//Pengalih isu
 
-    get data() {
-        return this._data
-    }
-
-    set data(value) {
-        this._data = value
-        this.save()
-    }
-
-    /**
-     * Queue Load
-     */
-    load() {
-        this._queue.push('_load')
-    }
-
-    /**
-     * Queue Save
-     */
-    save() {
-        this._queue.push('_save')
-    }
-
-    _load() {
-        try {
-          return this._data = existsSync(this.file) ? JSON.parse(readFileSync(this.file)) : {}
-        } catch (e) {
-          this.logger.error(e)
-          return this._data = {}
-        }
-    }
-
-    async _save() {
-        let dirname = _dirname(this.file)
-        if (!existsSync(dirname)) await fs.mkdir(dirname, { recursive: true })
-        await fs.writeFile(this.file, JSON.stringify(this._data, ...this._jsonargs))
-        return this.file
-    }
+for (let i = 0; i < loadd.length; i++) {
+await conn.sendMessage(m.chat, {text: loadd[i], edit: key })}
+await conn.sendMessage(m.chat, {
+text: kon,
+contextInfo: {
+externalAdReply: {
+showAdAttribution: true,
+title: ``,
+body: 'U S E R  I N Z I E',
+thumbnailUrl: 'https://telegra.ph/file/0d444923219cf5fe1733e.jpg',
+sourceUrl: "https://whatsapp.com/channel/0029Vajc7fkBA1erCyF48I3k",
+mediaType: 1,
+renderLargerThumbnail: true
+}}}, { quoted: reyz})
 }
+handler.help = ['user']
+handler.tags = ['info']
+handler.command = /^(pengguna|(jumlah)?database|user)$/i
 
-export default Database
+export default handler
